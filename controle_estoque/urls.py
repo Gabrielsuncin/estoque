57ffc55f0ea7d1ee35a81a48e3f586bae7b5e8e3
Gabriel Suncin <gc.everit@itmss.com.br>
2020-09-25 16:36:15 -0300

    upload index e principal, criada nova view

--- controle_estoque/urls.py
+++ controle_estoque/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from controle_estoque import urls as estoque_urls
+from django.urls import path
+from .views import principal
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(estoque_urls))
+    path('', principal),
+
 ]
