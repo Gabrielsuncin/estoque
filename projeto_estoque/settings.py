64d4f6929bed0dc80b3c8149f4433cb2afda00a1
Amauri <amaurirg@terra.com.br>
2020-09-27 21:51:23 -0300

    Deleted comment

--- projeto_estoque/settings.py
+++ projeto_estoque/settings.py
@@ -21,14 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '#h%^qjbqw2u1at$q%=otnuy67jr3w&(2y-tga=acbb=mv4hzru'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 ALLOWED_HOSTS = []
 
