93799622a32cb478ed65bd3632160d1ca010aa99
Amauri <amaurirg@terra.com.br>
2020-09-27 21:48:42 -0300

    Included decouple

--- projeto_estoque/settings.py
+++ projeto_estoque/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#h%^qjbqw2u1at$q%=otnuy67jr3w&(2y-tga=acbb=mv4hzru'
+# SECRET_KEY = '#h%^qjbqw2u1at$q%=otnuy67jr3w&(2y-tga=acbb=mv4hzru'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
